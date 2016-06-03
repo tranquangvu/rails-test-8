a17183bf53d7ee0a289f964192e7a3d920bcf980
tranquangvu <vutran.513@gmail.com>
2016-06-03 09:03:01 +0700

    complete code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
-	def sort_product_listing(params)
+	def sort_product_listing
+		options = params.dup
+		options[:product_filter] = options[:product_filter].except(:sort, :order) if options[:product_filter].present?
+
 		[
-			["Name (A-Z)", root_path(params.deep_merge(product_filter: {sort: :name, order: :asc}))],
-			["Name (Z-A)", root_path(params.deep_merge(product_filter: {sort: :name, order: :desc}))]
+			["Sort", root_path(options)],
+			["Name (A-Z)", root_path(options.deep_merge(product_filter: {sort: :name, order: :asc}))],
+			["Name (Z-A)", root_path(options.deep_merge(product_filter: {sort: :name, order: :desc}))]
 		]
 	end
 end
